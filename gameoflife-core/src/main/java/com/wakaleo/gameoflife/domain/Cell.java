388d1a3436809f9097eec70eb14f81e77d451980
Sumeet Sawhney <sumeetsawhney25@gmail.com>
2017-01-11 15:00:46 -0500

    added a test comment

--- gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
+++ gameoflife-core/src/main/java/com/wakaleo/gameoflife/domain/Cell.java
@@ -3,6 +3,8 @@ package com.wakaleo.gameoflife.domain;
 public enum Cell {
     LIVE_CELL("*"), DEAD_CELL(".");
 
+	//test comment
+	
     private String symbol;
 
     private Cell(final String initialSymbol) {
